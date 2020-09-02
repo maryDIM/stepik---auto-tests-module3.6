1d55b8e192ed91e784b59717834d0f3566cad849
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-02 10:00:29 +0300

    Поправила подсказку по языку

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="None",
-                     help="Choose language: ru or es")
+                     help="Choose language: ru, es, en...")
 
 
 @pytest.fixture(scope="function")
